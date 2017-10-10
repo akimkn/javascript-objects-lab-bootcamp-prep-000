0277a9f1e91e11b1ea0b3f0d6a27140ac9ca472b
Austin Kim <ask199416@gmail.com>
2017-10-10 06:31:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return obj
 }
 function deleteFromObjectByKey(obj, key) {
-  delete obj.key
-  return obj
+  var result = obj
+  
 }
